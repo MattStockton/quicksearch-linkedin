5d27d75f164ed537922d45965fd894b780ddd297
Matt Stockton <mstockton@harqen.com>
2013-08-27 17:04:17 -0500

    Adding omnibox support, with a keyword of _LI

--- chrome_extension/search.js
+++ chrome_extension/search.js
@@ -1,42 +1,51 @@
-var linked_in_search_handler = function(info, tab) {
-	if (info.menuItemId == "linkedin_search") {
-		var first_name = "";
-		var last_name = "";
-	  
-		if(info.selectionText){
-			var split_text = info.selectionText.match(/[^ ]+/g);
-			if(split_text.length >= 2){
-				first_name = split_text[0];
-				split_text.splice(0,1);
-				last_name = split_text.join();
-			}
+var linked_in_search = function(searchText){
+	var first_name = "";
+	var last_name = "";
+  
+	if(searchText){
+		var split_text = searchText.match(/[^ ]+/g);
+		if(split_text.length >= 2){
+			first_name = split_text[0];
+			split_text.splice(0,1);
+			last_name = split_text.join();
 		}
-	
-		var url = "http:localhost:5000/search.html";
-		//http://linkedinsearch.s3-website-us-east-1.amazonaws.com/v1.0/search.html
-	  
-		if(first_name && last_name){
-			url += "?first_name=" + encodeURIComponent(first_name)   +  "&last_name=" + encodeURIComponent(last_name);
-		} else {
-			url += "?invalid_search=true";
-		}
+	}
 
-		chrome.tabs.create({
-			url: url,
-			active: false
-		}, function(tab) {
-			chrome.windows.create({
-				tabId: tab.id,
-				width: 425,
-				height: 400,
-				type: 'panel',
-				focused: true
-			});
-		}); 
+	var url = "http:localhost:5000/search.html";
+	//http://linkedinsearch.s3-website-us-east-1.amazonaws.com/v1.0/search.html
+  
+	if(first_name && last_name){
+		url += "?first_name=" + encodeURIComponent(first_name)   +  "&last_name=" + encodeURIComponent(last_name);
+	} else {
+		url += "?invalid_search=true";
+	}
+
+	chrome.tabs.create({
+		url: url,
+		active: false
+	}, function(tab) {
+		chrome.windows.create({
+			tabId: tab.id,
+			width: 425,
+			height: 400,
+			type: 'panel',
+			focused: true
+		});
+	}); 
+}
+
+var linked_in_omnibox_handler = function(text, tab){
+	linked_in_search(text);
+}
+
+var linked_in_context_handler = function(info, tab) {
+	if (info.menuItemId == "linkedin_search") {
+		linked_in_search(info.selectionText);
 	}
 }
 
-chrome.contextMenus.onClicked.addListener(linked_in_search_handler);
+chrome.omnibox.onInputEntered.addListener(linked_in_omnibox_handler)
+chrome.contextMenus.onClicked.addListener(linked_in_context_handler);
 
 chrome.runtime.onInstalled.addListener(function() {
 	var context = "selection";
