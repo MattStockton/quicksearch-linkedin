9b8a38332eb88c73acfb6c1d128b970909d6726a
Matt Stockton <mstockton@harqen.com>
2013-08-27 15:41:41 -0500

    Fixing indentation, adding LinkedIn logo, and handling bad searches

--- chrome_extension/search.js
+++ chrome_extension/search.js
@@ -1,42 +1,45 @@
-function linkedInSearchHandler(info, tab) {
-  if (info.menuItemId == "linkedin_search") {
-	  var first_name = "";
-	  var last_name = "";
+var linked_in_search_handler = function(info, tab) {
+	if (info.menuItemId == "linkedin_search") {
+		var first_name = "";
+		var last_name = "";
 	  
-	  if(info.selectionText){
-		  var split_text = info.selectionText.split(" ");
-		  if(split_text.length >= 2){
-			  first_name = split_text[0];
-			  split_text.splice(0,1);
-			  last_name = split_text.join();
-		  }
-	  }
+		if(info.selectionText){
+			var split_text = info.selectionText.match(/[^ ]+/g);
+			if(split_text.length >= 2){
+				first_name = split_text[0];
+				split_text.splice(0,1);
+				last_name = split_text.join();
+			}
+		}
+	
+		var url = "http:localhost:5000/search.html";
+		//http://linkedinsearch.s3-website-us-east-1.amazonaws.com/v1.0/search.html
 	  
-	  if(first_name && last_name){
-		  //http://linkedinsearch.s3-website-us-east-1.amazonaws.com/v1.0/search.html
-		  var url = "http:localhost:5000/search.html?first_name=" + encodeURIComponent(first_name)   +  "&last_name=" + encodeURIComponent(last_name);
-		  chrome.tabs.create({
-		              url: url,
-					  active: false
-				  }, function(tab) {
-		              // After the tab has been created, open a window to inject the tab
-		              chrome.windows.create({
-		                  tabId: tab.id,
-						  width: 425,
-						  height: 400,
-		                  type: 'panel',
-		                  focused: true
-		                  // incognito, top, left, ...
-		              });
-		          }); 
-	  }
-  }
-};
+		if(first_name && last_name){
+			url += "?first_name=" + encodeURIComponent(first_name)   +  "&last_name=" + encodeURIComponent(last_name);
+		} else {
+			url += "?invalid_search=true";
+		}
 
-chrome.contextMenus.onClicked.addListener(linkedInSearchHandler);
+		chrome.tabs.create({
+			url: url,
+			active: false
+		}, function(tab) {
+			chrome.windows.create({
+				tabId: tab.id,
+				width: 425,
+				height: 400,
+				type: 'panel',
+				focused: true
+			});
+		}); 
+	}
+}
+
+chrome.contextMenus.onClicked.addListener(linked_in_search_handler);
 
 chrome.runtime.onInstalled.addListener(function() {
-    var context = "selection";
-    var id = chrome.contextMenus.create({"title": "Search LinkedIn", "contexts":[context],
-                                         "id": "linkedin_search"});
+	var context = "selection";
+	var id = chrome.contextMenus.create({"title": "Search LinkedIn for '%s'", "contexts":[context],
+	"id": "linkedin_search"});
 });
