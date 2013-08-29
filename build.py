b511f6c3efb18d57b159a9afadd97cc2da13f887
Matt Stockton <mstockton@harqen.com>
2013-08-29 15:14:44 -0500

    Simplifying build, and handling missing data for LinkedIn search callback

--- build.py
+++ build.py
@@ -5,43 +5,39 @@ from os.path import isfile, join, basename
 from boto.s3.bucket import Bucket
 from boto.s3.key import Key
 from boto.s3.connection import S3Connection
-   
+ 
+# configuration.json holds our config and template values
 json_data = open('configuration.json')
 data = json.load(json_data)
+build_dir = './build/'
 
 # Remove build if it exists
 try:
-    shutil.rmtree('./build')
+    shutil.rmtree(build_dir)
 except:
     pass
     
-directories = ['chrome_extension', 'files_to_host']
 templatable = ['.html', '.json', '.js']
 
 # Quick and dirty mechanism to copy files to ./build while replacing the appropriate values from configuration.json
-print 'Starting to copy files to the build directory...'
-for cur_dir in directories:
-    new_dir = './build/' + cur_dir
-    os.makedirs(new_dir)
-    files = [ join(cur_dir,f) for f in os.listdir(cur_dir) if isfile(join(cur_dir,f)) ]
-    
+print 'Copying files to the build directory...'
+shutil.copytree(src='.',dst=build_dir)
+
+# Loop through all of the files in build, replacing templated values where necessary
+for root, dirs, files in os.walk(build_dir):
     for file in files:
+        full_path = os.path.join(root, file)
         file_name, file_extension = os.path.splitext(file)
-  
         if file_extension in templatable:
             print 'Templating ' + file + ' with configuration.json values'
-            with codecs.open(file, 'r', 'utf-8') as cur_file:
+            with codecs.open(full_path, 'r+', 'utf-8') as cur_file:
                 file_data = cur_file.read()
                 for cur_key, cur_value in data.iteritems():
                     file_data = file_data.replace("{{" + cur_key + "}}", cur_value)
-                
-                new_file = codecs.open(join(new_dir,basename(file)), 'w', 'utf-8')
-                new_file.write(file_data)
-                new_file.close()
-        else:
-            print 'Copying ' + file + ' without templating'
-            shutil.copyfile(file, join(new_dir,basename(file)))
-            
+                    
+                cur_file.seek(0)
+                cur_file.truncate()
+                cur_file.write(file_data)                  
 
 deploy_aws = data.get('deploy_aws', "False") == "True"
 aws_key = data.get('aws_key', None)
